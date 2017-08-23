ba0b5c48bfcc9b2fb695cb0dd6bd1533911abd41
Gabriel Moreira <gabrielstrzal@gmail.com>
2017-08-23 17:20:32 -0300

    added obstacle

--- core/src/com/flappy/game/states/PlayState.java
+++ core/src/com/flappy/game/states/PlayState.java
@@ -5,17 +5,20 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.flappy.game.Flappy;
 import com.flappy.game.sprites.Bird;
+import com.flappy.game.sprites.Tube;
 
 
 public class PlayState extends State {
     private Bird bird;
     private Texture bg;
+    private Tube tube;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50,300);
         cam.setToOrtho(false, Flappy.WIDTH/2, Flappy.HEIGHT/2);
         bg = new Texture("bg.png");
+        tube = new Tube(100);
     }
 
     @Override
@@ -38,6 +41,8 @@ public class PlayState extends State {
         sb.begin();
         sb.draw(bg, cam.position.x- (cam.viewportWidth/2),0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTupe(),tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         sb.end();
     }
 
